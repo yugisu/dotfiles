4aee15f4af3a3915db6835c569b8128d15c893e6
Dmitri Puzyr <dmitri.puzyr@fluxon.com>
2023-10-29 23:50:10 +0200

    Fix missing quote in homebrew.sh

--- homebrew.sh
+++ homebrew.sh
@@ -5,7 +5,7 @@ if test ! $(which brew); then
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
     (echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> $HOME/.zprofile
-    eval "$(/opt/homebrew/bin/brew shellenv)
+    eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
 
 brew update
